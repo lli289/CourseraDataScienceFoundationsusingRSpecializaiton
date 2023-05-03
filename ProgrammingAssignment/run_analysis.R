7436f0ffa6491f5b48900942ea71d61c2c2192fd
Lillian Li <lli289.git@gmail.com>
2023-05-03 05:27:32 -0400

    Create run_analysis.R

---
+++ ProgrammingAssignment/run_analysis.R
@@ -0,0 +1,42 @@
+# load data
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+
+# step 1
+A <- rbind(x_train, x_test)
+B <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+Merged_Data <- cbind(Subject, B, A)
+
+# step 2
+tidy <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+
+# step 3
+tidy$code <- activities[tidy$code, 2]
+
+# step 4
+names(tidy)[2] = "activity"
+names(tidy)<-gsub("Acc", "Accelerometer", names(tidy))
+names(tidy)<-gsub("Gyro", "Gyroscope", names(tidy))
+names(tidy)<-gsub("BodyBody", "Body", names(tidy))
+names(tidy)<-gsub("Mag", "Magnitude", names(tidy))
+names(tidy)<-gsub("^t", "Time", names(tidy))
+names(tidy)<-gsub("^f", "Frequency", names(tidy))
+names(tidy)<-gsub("tBody", "TimeBody", names(tidy))
+names(tidy)<-gsub("-mean()", "Mean", names(tidy), ignore.case = TRUE)
+names(tidy)<-gsub("-std()", "STD", names(tidy), ignore.case = TRUE)
+names(tidy)<-gsub("-freq()", "Frequency", names(tidy), ignore.case = TRUE)
+names(tidy)<-gsub("angle", "Angle", names(tidy))
+names(tidy)<-gsub("gravity", "Gravity", names(tidy))
+
+# step 5
+data <- tidy %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(data, "tidy.txt", row.name=FALSE)
